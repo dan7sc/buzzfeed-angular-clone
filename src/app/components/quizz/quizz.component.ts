b40a7e7db35a90780026318adce72a2345efdcaf
dan7sc <dan7sc@users.noreply.github.com>
2024-09-02 16:12:00 -0300

    add game rules

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,14 +1,16 @@
-import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { NgIf, NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json';
+
 
 @Component({
   selector: 'app-quizz',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, NgFor],
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
   title: string = ""
 
   questions: any
@@ -21,4 +23,32 @@ export class QuizzComponent {
   questionMaxIndex: number = 0
 
   finished: boolean = false
+
+  ngOnInit(): void {
+    if(quizz_questions) {
+      this.finished = false
+      this.title = quizz_questions.title
+
+      this.questions = quizz_questions.questions
+      this.selectedQuestion = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+    }
+  }
+
+  playerChoose(value: string) {
+    this.answers.push(value)
+    this.nextStep()
+  }
+
+  async nextStep() {
+    this.questionIndex += 1
+
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.selectedQuestion = this.questions[this.questionIndex]
+    } else {
+      this.finished = true
+    }
+  }
 }
