fa84503351fabaa9edb8e5ee11f3fe550bdea4e7
dan7sc <dan7sc@users.noreply.github.com>
2024-09-02 16:44:21 -0300

    show answer

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -48,7 +48,24 @@ export class QuizzComponent implements OnInit {
     if (this.questionMaxIndex > this.questionIndex) {
       this.selectedQuestion = this.questions[this.questionIndex]
     } else {
+      const finalAnswer: string = await this.checkResult(this.answers)
       this.finished = true
+      this.selectedAnswer = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results]
     }
+  }
+
+  async checkResult(answers: string[]) {
+    const result = answers.reduce((previous, current, i, arr) => {
+      if (
+        arr.filter(item => item === previous).length >
+        arr.filter(item => item === current).length
+      ) {
+        return previous
+      } else {
+        return current
+      }
+    })
+
+    return result
   }
 }
