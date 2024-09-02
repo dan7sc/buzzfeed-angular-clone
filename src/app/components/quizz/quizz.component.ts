92c3abc00264a85ff6dcedef87a8c1968492b952
dan7sc <dan7sc@users.noreply.github.com>
2024-09-02 16:11:22 -0300

    add business logic

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,12 +1,24 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-quizz',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
 export class QuizzComponent {
+  title: string = ""
 
+  questions: any
+  selectedQuestion: any
+
+  answers: string[] = []
+  selectedAnswer: string = ""
+
+  questionIndex: number = 0
+  questionMaxIndex: number = 0
+
+  finished: boolean = false
 }
